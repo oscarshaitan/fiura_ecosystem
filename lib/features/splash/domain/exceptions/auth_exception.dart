c577d37c0fede03d2baf73b6a531a90867baf276
jgil23 <jgil@el-lab.co>
2022-12-03 14:18:07 -0500

    Unused files and code deleted

--- lib/features/splash/domain/exceptions/auth_exception.dart
+++
@@ -1,9 +0,0 @@
-enum AuthErrorCode {
-  notAuth,
-}
-
-class AuthException implements Exception {
-  AuthException(this.error);
-
-  final AuthErrorCode error;
-}
--- lib/features/splash/presentation/cubit/splash_cubit.dart
+++ lib/features/splash/presentation/cubit/splash_cubit.dart
@@ -1,8 +1,5 @@
 import 'package:fiura_ecosystem/features/splash/domain/use_cases/get_splash_use_case.dart';
-import 'package:fiura_ecosystem/router/app_router.gr.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../domain/exceptions/auth_exception.dart';
 
 part 'splash_state.dart';
 
@@ -13,17 +10,6 @@ class Splashcubit extends Cubit<SplashState> {
 
   final GetSplashUseCase _getSplashUseCase;
 
-  // void init() async {
-  //   try {
-  //     final result = await _getSplashUseCase.getUser();
-  //   } on AuthException catch (e) {
-  //     if (e.error == AuthErrorCode.notAuth) {
-  //       emit(SplashState.none);
-  //     } else {
-  //       emit(SplashState.logedUser);
-  //     }
-  //   }
-  // }
   void init() async {
     final result = await _getSplashUseCase.getUser();
 
