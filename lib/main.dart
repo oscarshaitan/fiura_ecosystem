a2e4c893b764cbecde0bc6051dc4e75765c21a7f
jgildevelop <jgil@gethava.com>
2023-02-15 20:42:49 -0500

    Firebase dependencies and config

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fiura_ecosystem/dependencies.dart';
+import 'package:fiura_ecosystem/firebase_options.dart';
 import 'package:fiura_ecosystem/router/app_router.gr.dart';
 import 'package:fiura_ecosystem/theming.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setPathUrlStrategy();
   setup();
   runApp(FiuraApp());
