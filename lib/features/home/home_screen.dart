d184dac0607249114411eba3946122b1c2d33d4e
johangv <37637095+johangv@users.noreply.github.com>
2023-09-03 12:15:02 -0500

    Cambios del 3 de Septiembre (#13)
    * advances on update artist, it has bugs that should be fixed
    
    * Artist edit and delete finished
    
    * Empty artist list solved adding EmptyListWidget
    
    * bug fixes
    
    * comment deleted

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:fiura_ecosystem/features/home/cubit/session_cubit.dart';
 import 'package:fiura_ecosystem/features/home/widgets/logout_modal.dart';
+import 'package:fiura_ecosystem/features/widgets/on_load_message.dart';
 import 'package:fiura_ecosystem/router/app_router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -26,6 +27,7 @@ class HomeScreen extends StatelessWidget {
                     child: DrawerHeader(
                         decoration: const BoxDecoration(color: Colors.red),
                         child: sessionState.maybeMap(
+                          error: (value) => const OnLoadMessage(),
                           userFetched: (state) {
                             List<String> nameList =
                                 state.currentUser!.name.split(' ');
--- lib/features/home/repository/user_respository.dart
+++ lib/features/home/repository/user_respository.dart
@@ -27,9 +27,7 @@ class UserRepository {
   Future<void> deleteUser() async {
     if (auth.currentUser != null) {
       try {
-        await db.collection('users').doc(auth.currentUser!.uid).delete();
         await auth.currentUser!.delete();
-        auth.signOut();
         googleSignIn.signOut();
       } catch (e) {
         throw Exception('Error eliminando el usuario');
