ecbe5d3469408a1a0d6696e2ee0947379bba437d
oscar <oscar@symterra.co.uk>
2023-09-24 14:28:02 +0100

    app integrity

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fiura/dependencies.dart';
 import 'package:fiura/firebase_options.dart';
 import 'package:fiura/router/app_router.gr.dart';
 import 'package:fiura/theming.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
@@ -12,6 +13,10 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
+  );
+
   setPathUrlStrategy();
   setup();
   runApp(FiuraApp());
